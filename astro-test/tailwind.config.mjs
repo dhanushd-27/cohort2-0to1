f67662f1e0ba0cb95302371bc191f6705946311d
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2025-01-02 12:14:07 +0530

    Learn animations with astro and tailwind

--- astro-test/tailwind.config.mjs
+++ astro-test/tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+
+			},
+			keyframes: {
+				'trans-right': {
+					'0%, 100%': { transform: 'translateX(300px) translateY(1000px)'},
+					'50%': { transform: 'translateX(0px) translateY(0px)' }
+				}
+			},
+			animation: {
+				'trans-right': 'trans-right 4.5s ease-out infinite'
+			},
+			blur: {
+				'white-bg-blur': '30'
+			}
+		},
 	},
 	plugins: [],
 }
